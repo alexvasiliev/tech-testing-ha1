541c636e0bc0ff4f8e71f6997c82731a2056f643
user <user@user.ru>
2014-10-06 18:13:02 +0400

    some tests

--- provision/init.lua
+++ provision/init.lua
@@ -1,4 +1,4 @@
--- vim: set ft=lua et :
+lu-- vim: set ft=lua et :
 
 --  The library is free software; you can redistribute it and/or modify it
 --  under the terms of either:
--- source/tests/test_notification_pusher.py
+++ source/tests/test_notification_pusher.py
@@ -1,15 +1,42 @@
 import unittest
 import mock
-from notification_pusher import create_pidfile
-
+import  tarantool
+from source import notification_pusher
 
 class NotificationPusherTestCase(unittest.TestCase):
-    def test_create_pidfile_example(self):
-        pid = 42
-        m_open = mock.mock_open()
-        with mock.patch('notification_pusher.open', m_open, create=True):
-            with mock.patch('os.getpid', mock.Mock(return_value=pid)):
-                create_pidfile('/file/path')
+    def test_stop_handler(self):
+        notification_pusher.run_application = True
+        exit_code = 42
+        notification_pusher.stop_handler(exit_code)
+        self.assertFalse(notification_pusher.run_application)
 
-        m_open.assert_called_once_with('/file/path', 'w')
-        m_open().write.assert_called_once_with(str(pid))
+    def test_done_with_processed_tasks_succ(self):
+        task = mock.Mock()
+        task_queue = mock.Mock()
+        task_queue.qsize = mock.Mock(return_value = 1)
+        task_queue.get_nowait = mock.Mock(return_value = (task, 'action'))
+
+        m_logger = mock.MagicMock()
+        with mock.patch('notification_pusher.logger', m_logger):
+            notification_pusher.done_with_processed_tasks(task_queue)
+        self.assertFalse(m_logger.exception.called)
+
+    def test_done_with_processed_tasks_err(self):
+        task = mock.Mock()
+        task.task_method = mock.Mock(side_effect=tarantool.DatabaseError())
+        task_queue = mock.Mock()
+        task_queue.qsize = mock.Mock(return_value = 1)
+        task_queue.get_nowait = mock.Mock(return_value=(task, 'task_method'))
+        try:
+            notification_pusher.done_with_processed_tasks(task_queue)
+        except tarantool.DatabaseError:
+            self.fail()
+
+    def test_done_with_processed_tasks_empty_list(self):
+        task_queue = mock.Mock()
+        task_queue.qsize = mock.Mock(return_value = 1)
+        task_queue.get_nowait = mock.Mock(side_effect = notification_pusher.gevent_queue.Empty)
+        try:
+            notification_pusher.done_with_processed_tasks(task_queue)
+        except notification_pusher.gevent_queue.Empty:
+			self.fail()
