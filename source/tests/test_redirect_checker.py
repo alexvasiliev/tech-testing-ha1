4eff29e3a8fd6bf0324405f1c01cc255e950507b
user <user@user.ru>
2014-10-07 16:02:17 +0400

    redirect_checker 0.9 done

--- source/tests/test_redirect_checker.py
+++ source/tests/test_redirect_checker.py
@@ -6,7 +6,6 @@ from argparse import Namespace
 def stop_redirect_checker(*smth):
     redirect_checker.isRunning = False
 
-
 def start_redirect_checker(*smth):
     redirect_checker.isRunning = True
 
@@ -29,7 +28,7 @@ class RedirectCheckerTestCase(unittest.TestCase):
                         redirect_checker.main_loop(self.config)
         assert workd.called
 
-    def test_main_loop_with_network_status_fail(self):
+    def test_main_loop_with_network_status_fail_and_normal_worker_counts(self):
         pid = 42
         children_mock = mock.Mock()
         with mock.patch('os.getpid', mock.Mock(return_value=pid)):
@@ -47,12 +46,56 @@ class RedirectCheckerTestCase(unittest.TestCase):
                 with mock.patch('source.redirect_checker.check_network_status', mock.Mock(return_value=True)):
                     with mock.patch('source.redirect_checker.spawn_workers', mock.Mock()) as workd:
                         redirect_checker.main_loop(self.config)
-
         assert not workd.called
 
-    
+    def test_main_with_demon_no_pidfile(self):
+        setattr(self.config, "LOGGING", {"version": 1})
+        setattr(self.config, "EXIT_CODE", 0)
+        with mock.patch('source.redirect_checker.daemonize', mock.Mock()) as daemon:
+            with mock.patch('source.redirect_checker.load_config_from_pyfile', mock.Mock(return_value=self.config)):
+                with mock.patch('os.path', mock.MagicMock(return_value=self.config)):
+                    with mock.patch('source.redirect_checker.parse_cmd_args', mock.Mock(return_value=Namespace(daemon=True, config=self.config, pidfile=None))):
+                        with mock.patch('source.redirect_checker.create_pidfile', mock.Mock()) as pidfile:
+                            redirect_checker.main([])
+        assert daemon.called
+        assert not pidfile.called
 
+    def test_main_no_demon_with_pidfile(self):
+        setattr(self.config, "LOGGING", {"version": 1})
+        setattr(self.config, "EXIT_CODE", 0)
+        with mock.patch('source.redirect_checker.daemonize', mock.Mock()) as daemon:
+            with mock.patch('source.redirect_checker.load_config_from_pyfile', mock.Mock(return_value=self.config)):
+                with mock.patch('os.path', mock.MagicMock(return_value=self.config)):
+                    with mock.patch('source.redirect_checker.parse_cmd_args', mock.Mock(return_value=Namespace(daemon=False, config=self.config, pidfile=1))):
+                        with mock.patch('source.redirect_checker.create_pidfile', mock.Mock()) as pidfile:
+                            redirect_checker.main([])
+        assert not daemon.called
+        assert pidfile.called
 
+    def test_main_with_demon_with_pidfile(self):
+        exitcode = 322
+        setattr(self.config, "LOGGING", {"version": 1})
+        setattr(self.config, "EXIT_CODE", exitcode)
+        with mock.patch('source.redirect_checker.daemonize', mock.Mock()) as daemon:
+            with mock.patch('source.redirect_checker.load_config_from_pyfile', mock.Mock(return_value=self.config)):
+                with mock.patch('os.path', mock.MagicMock(return_value=self.config)):
+                    with mock.patch('source.redirect_checker.parse_cmd_args', mock.Mock(return_value=Namespace(daemon=True, config=self.config, pidfile=1))):
+                        with mock.patch('source.redirect_checker.create_pidfile', mock.Mock()) as pidfile:
+                            assert redirect_checker.main([]) == exitcode
+        assert daemon.called
+        assert pidfile.called
+
+    def test_main_no_demon_no_pidfile(self):
+        setattr(self.config, "LOGGING", {"version": 1})
+        setattr(self.config, "EXIT_CODE", 0)
+        with mock.patch('source.redirect_checker.daemonize', mock.Mock()) as daemon:
+            with mock.patch('source.redirect_checker.load_config_from_pyfile', mock.Mock(return_value=self.config)):
+                with mock.patch('os.path', mock.MagicMock(return_value=self.config)):
+                    with mock.patch('source.redirect_checker.parse_cmd_args', mock.Mock(return_value=Namespace(daemon=False, config=self.config, pidfile=0))):
+                        with mock.patch('source.redirect_checker.create_pidfile', mock.Mock()) as pidfile:
+                            redirect_checker.main([])
+        assert not daemon.called
+        assert not pidfile.called
 
     def tearDown(self):
         start_redirect_checker()
