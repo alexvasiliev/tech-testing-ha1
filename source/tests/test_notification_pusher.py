4a85de5b45e614ec7fb0f707973a7ad76c404b29
user <user@user.ru>
2014-10-07 18:16:33 +0400

    added some cooperation with mike xD

--- source/tests/test_notification_pusher.py
+++ source/tests/test_notification_pusher.py
@@ -24,8 +24,7 @@ class NotificationPusherTestCase(unittest.TestCase):
 
     def test_stop_handler(self):
         notification_pusher.run_application = True
-        exit_code = 0
-        notification_pusher.stop_handler(exit_code)
+        notification_pusher.stop_handler(notification_pusher.exit_code)
         self.assertFalse(notification_pusher.run_application)
 
     def test_done_with_processed_tasks_suc(self):
@@ -33,11 +32,10 @@ class NotificationPusherTestCase(unittest.TestCase):
         task_queue = mock.Mock()
         task_queue.qsize = mock.Mock(return_value = 1)
         task_queue.get_nowait = mock.Mock(return_value = (task, 'action'))
-
-        m_logger = mock.MagicMock()
-        with mock.patch('notification_pusher.logger', m_logger):
+        logger = mock.MagicMock()
+        with mock.patch('notification_pusher.logger', logger):
             notification_pusher.done_with_processed_tasks(task_queue)
-        self.assertFalse(m_logger.exception.called)
+        self.assertFalse(logger.exception.called)
 
     def test_done_with_processed_tasks_err(self):
         task = mock.Mock()
@@ -61,64 +59,25 @@ class NotificationPusherTestCase(unittest.TestCase):
 			self.fail()
 
     def test_notification_worker_ack(self):
-
         data = {'callback_url': 'url'}
         task = mock.Mock()
         task.data.copy = mock.Mock(return_value=data)
         task_queue = mock.MagicMock()
         response = mock.Mock(return_value={'status_code': 200})
-
         with mock.patch.object(json, 'dumps', mock.Mock()), mock.patch('source.notification_pusher.requests.post',
                            mock.Mock(return_value=response)):
             notification_pusher.notification_worker(task, task_queue)
-
         task_queue.put.assert_called_with((task, 'ack'))
 
     def test_notification_worker_bury(self):
-
         data = {'callback_url': 'url'}
         task = mock.Mock()
         task.data.copy = mock.Mock(return_value=data)
         task_queue = mock.MagicMock()
-
         with mock.patch.object(json, 'dumps', mock.Mock()), mock.patch('source.notification_pusher.requests.post',
                            mock.Mock(side_effect=notification_pusher.requests.RequestException("exc"))):
             notification_pusher.notification_worker(task, task_queue)
         task_queue.put.assert_called_with((task, 'bury'))
-
-    def test_demonize_parent(self):
-        pid = 42
-        with mock.patch('os.fork', mock.Mock(return_value=pid)):
-            with mock.patch('os._exit', mock.Mock()) as os_exit:
-                notification_pusher.daemonize()
-        os_exit.assert_called_once_with(0)
-
-    def test_demonize_child_parent(self):
-        pid = 42
-        with mock.patch('os.fork', mock.Mock(side_effect=[0, pid])):
-            with mock.patch('os.setsid', mock.Mock()):
-                with mock.patch('os._exit', mock.Mock()) as os_exit:
-                    notification_pusher.daemonize()
-        os_exit.assert_called_once_with(0)
-
-    def test_demonize_child(self):
-        pid = 0
-        with mock.patch('os.fork', mock.Mock(return_value=pid)) as os_fork:
-            with mock.patch('os._exit', mock.Mock()) as os_exit:
-                with mock.patch('os.setsid', mock.Mock()) as os_setsid:
-                    notification_pusher.daemonize()
-        self.assertTrue(os_fork.called)
-        self.assertTrue(os_setsid.called)
-        self.assertFalse(os_exit.called)
-
-    def test_demonize_failed_first_fork(self):
-        with mock.patch('os.fork', mock.Mock(side_effect=OSError("Error"))):
-            self.assertRaises(Exception, notification_pusher.daemonize)
-
-    def test_demonize_failed_second_fork(self):
-        with mock.patch('os.fork', mock.Mock(side_effect=[0, OSError("Error")])):
-            with mock.patch('os.setsid', mock.Mock()):
-                self.assertRaises(Exception, notification_pusher.daemonize)
 
     def test_mainloop_conf_succ(self):
         task_queue = mock.Mock(side_effect = stop_notification_pusher)
@@ -136,8 +95,22 @@ class NotificationPusherTestCase(unittest.TestCase):
 
     def test_mainloop_worker_succ(self):
         worker = mock.Mock()
-        with mock.patch('tarantool_queue.tarantool_queue.Tube.take', mock.MagicMock()) as taker,\
-             mock.patch('gevent.Greenlet', mock.Mock(return_value=worker)):
+        with mock.patch('gevent.Greenlet', mock.Mock(return_value=worker)):
             with mock.patch('notification_pusher.sleep', mock.Mock(side_effect=stop_notification_pusher)):
                 notification_pusher.main_loop(self.config)
         worker.start.assert_called_once()
+
+    def test_mainloop_app_no_run(self):
+        m_logger_info = mock.MagicMock()
+        notification_pusher.run_application = False
+        with mock.patch('notification_pusher.logger.info', m_logger_info):
+            notification_pusher.main_loop(self.config)
+        m_logger_info.assert_called_with('Stop application loop.')
+        notification_pusher.run_application = True
+
+    def test_install_signal_handlers(self):
+        with mock.patch('gevent.signal', mock.Mock()) as signal:
+            notification_pusher.install_signal_handlers()
+        assert signal.called
+
+
